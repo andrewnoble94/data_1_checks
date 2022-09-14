3b106da0b4d31b74ead4d65318808a6a17430c44
Andrew <andrew.n@reachleft.com>
2022-09-14 19:33:11 -0400

    first commit

---
+++ 9-14.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+
+my_list = ['1', '2', '3']
+
+print(my_list[0])
+
+my_dict = {'key1' : 'value1', 'key2' : 'value2'}
+
+print(my_dict['key1'])
+
+my_tuple = (1,2,3,4)
+
+print(my_tuple[1])
